0eed1f8bd94b422bdeb8fcf121aaaa7022c544aa
Caleb Kopecky <calebjkopecky@gmail.com>
2021-02-24 18:53:29 -0600

    added new print line

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 public class Test{
 	public static void main(String[] args){
 		System.out.println("Testing");
+		System.out.println("think i got it now");
 	}
 } // still learning how to use github
 //this line was added on eclipse
